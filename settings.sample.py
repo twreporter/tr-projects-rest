ed1fe67d047696ccea43fbd6af77b437d927d787
drumrick <me@drumrick.tw>
2015-11-19 06:47:59 +0800

    change to a smaller schema

--- settings.sample.py
+++ settings.sample.py
@@ -7,15 +7,6 @@ MONGO_DBNAME = ''
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
 schema = {
-  'xorgAvailability': {
-    'type': 'boolean',
-  },
-  'story_theme': {
-    'type': 'string',
-  },
-  'manual_slug_set': {
-    'type': 'boolean',
-  },
   'subtitle': {
     'type': 'string',
   },
@@ -25,75 +16,17 @@ schema = {
   'byline': {
     'type': 'string',
   },
-  'suppress_toolbars': {
-    'type': 'integer',
-  },
   'title': {
     'type': 'string',
-  },
-  'wv_availability': {
-    'type': 'integer',
-  },
-  'rev_of': {
-    'type': 'string',
-    'nullable': True,
   },
   'firstImage': {
     'type': 'string',
   },
-  'title_page': {
-    'type': 'string',
-  },
-  'minimum_version': {
-    'type': 'string',
-  },
-  'disable_swipe_to_store': {
-    'type': 'integer',
-  },
-  'suppress_jacket': {
-    'type': 'integer',
-  },
-  'soundtrack_default_state': {
-    'type': 'string',
-  },
-  'soundtrack_loop': {
-    'type': 'integer',
-  },
-  'for_sale': {
-    'type': 'string',
-  },
-  'publish_web': {
-    'type': 'integer',
-  },
-  'weight': {
-    'type': 'integer',
-  },
   'excerpt': {
     'type': 'string',
   },
-  'has_preview': {
-    'type': 'integer',
-  },
-  'lastUpdateBy': {
-    'type': 'string',
-  },
-  'has_1url': {
-    'type': 'integer',
-  },
   'preset_tags': {
     'type': 'list',
-  },
-  'story_template_prefix_pagination': {
-    'type': 'string',
-  },
-  'story_template_prefix_hyphenate': {
-    'type': 'string',
-  },
-  'last_cache': {
-    'type': 'integer',
-  },
-  'facebook_image': {
-    'type': 'string',
   },
   'thumbnail': {
     'type': 'string',
@@ -112,37 +45,16 @@ schema = {
   'tags': {
     'type': 'list',
   },
-  'javascript': {
-    'type': 'string',
-  },
   'author_display': {
     'type': 'string',
   },
   'slug': {
     'type': 'string',
   },
-  'preview_image': {
-    'type': 'string',
-  },
   'story_type': {
     'type': 'string',
   },
-  'soundtrack': {
-    'type': 'list',
-  },
-  'product_id': {
-    'type': 'string',
-  },
   'created': {
-    'type': 'string',
-  },
-  'suppress_text_size': {
-    'type': 'integer',
-  },
-  'story_template': {
-    'type': 'string',
-  },
-  'cover_partial': {
     'type': 'string',
   },
   'published': {
@@ -157,191 +69,8 @@ schema = {
   'author_id': {
     'type': 'string',
   },
-  'facebook_enable': {
-    'type': 'integer',
-  },
-  'twitter_enable': {
-    'type': 'integer',
-  },
-  'facebook_author_url': {
-    'type': 'string',
-  },
-  'story_link_enable': {
-    'type': 'integer',
-  },
-  'asin': {
-    'type': 'string',
-  },
-  'story_template_prefix_epub_download': {
-    'type': 'integer',
-  },
-  'title_page_ebook': {
-    'type': 'string',
-  },
-  'publish_epub': {
-    'type': 'integer',
-  },
-  'publish_ibooks': {
-    'type': 'integer',
-  },
-  'epubs': {
-    'type': 'dict',
-    'schema': {
-      'epub' : {
-        'type': 'string',
-      },
-      'ibooks' : {
-        'type': 'string',
-      },
-    },
-  },
-  'options': {
-    'type': 'dict',
-    'schema': {
-      'suppress_chapter_pointer': {
-        'type': 'integer',
-      },
-      'story_theme': {
-        'type': 'string',
-      },
-      'manual_slug_set': {
-        'type': 'boolean',
-      },
-      'cms2': {
-        'type': 'boolean',
-      },
-      'suppress_text_size': {
-        'type': 'integer',
-      },
-      'story_template_prefix_drop_caps': {
-        'type': 'string',
-      },
-      'story_template_prefix_hyphenate': {
-        'type': 'string',
-      },
-      'publish_apps': {
-        'type': 'integer',
-      },
-      'byline': {
-        'type': 'string',
-      },
-      'suppress_toolbars': {
-        'type': 'integer',
-      },
-      'story_template_prefix_pop-up_style': {
-        'type': 'string',
-      },
-      'wv_availability': {
-        'type': 'integer',
-      },
-      'story_template_prefix_pagination': {
-        'type': 'string',
-      },
-      'last_cache': {
-        'type': 'integer',
-      },
-      'metadata': {
-        'type': 'dict',
-        'schema': {
-          'metadata_text_key_name': {
-            'type': 'string',
-          },
-        }
-      },
-      'disable_swipe_to_store': {
-        'type': 'integer',
-      },
-      'cover_type': {
-        'type': 'string',
-      },
-      'story_template_prefix_map_style': {
-        'type': 'string',
-      },
-      'suppress_jacket': {
-        'type': 'integer',
-      },
-      'soundtrack_default_state': {
-        'type': 'string',
-      },
-      'use_profile_for_byline': {
-        'type': 'integer',
-      },
-      'soundtrack': {
-        'type': 'list',
-      },
-      'soundtrack_loop': {
-        'type': 'integer',
-      },
-      'commerce': {
-        'type': 'integer',
-      },
-      'story_template_prefix_navigation_option': {
-        'type': 'string',
-      },
-      'story_template_prefix_paragraph_spacing': {
-        'type': 'string',
-      },
-      'story_template': {
-        'type': 'string',
-      },
-      'cover_partial': {
-        'type': 'string',
-      },
-      'suppress_slideoutnav': {
-        'type': 'integer',
-      },
-      'publish_web': {
-        'type': 'integer',
-      },
-      'suppress_chapter_number_display': {
-        'type': 'integer',
-      },
-      'toc_icon': {
-        'type': 'integer',
-      },
-      'facebook_enable': {
-        'type': 'integer',
-      },
-      'twitter_enable': {
-        'type': 'integer',
-      },
-      'facebook_author_url': {
-        'type': 'string',
-      },
-      'story_link_enable': {
-        'type': 'integer',
-      },
-      'asin': {
-        'type': 'string',
-      },
-      'story_template_prefix_epub_download': {
-        'type': 'integer',
-      },
-      'title_page_ebook': {
-        'type': 'string',
-      },
-      'publish_epub': {
-        'type': 'integer',
-      },
-      'publish_ibooks': {
-        'type': 'integer',
-      },
-    },
-  },
-  'isbn': {
-    'type': 'string',
-  },
-  'cms2': {
-    'type': 'boolean',
-  },
   'author_list': {
     'type': 'list',
-  },
-  'story_template_prefix_drop_caps': {
-    'type': 'string',
-  },
-  'publish_apps': {
-    'type': 'integer',
   },
   'lastPublish': {
     'type': 'string',
@@ -349,22 +78,7 @@ schema = {
   'user_id': {
     'type': 'string',
   },
-  'stylesheet': {
-    'type': 'string',
-  },
   'square_image': {
-    'type': 'string',
-  },
-  'device_specific_options': {
-    'type': 'string',
-  },
-  'cover_type': {
-    'type': 'string',
-  },
-  'story_template_prefix_map_style': {
-    'type': 'string',
-  },
-  'organization_id': {
     'type': 'string',
   },
   'price_credits': {
@@ -373,26 +87,8 @@ schema = {
   'createdBy': {
     'type': 'string',
   },
-  'commerce': {
-    'type': 'integer',
-  },
-  'soundtrack_id': {
-    'type': 'string',
-  },
-  'suppress_chapter_number_display': {
-    'type': 'integer',
-  },
-  'toc_icon': {
-    'type': 'integer',
-  },
-  'suppress_chapter_pointer': {
-    'type': 'integer',
-  },
   'isPublishedVersion': {
     'type': 'boolean',
-  },
-  'story_template_prefix_paragraph_spacing': {
-    'type': 'string',
   },
   'facebook_text': {
     'type': 'string',
@@ -403,34 +99,16 @@ schema = {
   'atavist_id': {
     'type': 'integer',
   },
-  'story_template_prefix_pop-up_style': {
-    'type': 'string',
-  },
-  'store_thumbnail': {
-    'type': 'string',
-  },
-  'title_page_landscape': {
-    'type': 'string',
-  },
   'status': {
-    'type': 'string',
-  },
-  'exclude_subscriptions': {
-    'type': 'string',
-  },
-  'use_profile_for_byline': {
-    'type': 'integer',
-  },
-  'suppress_slideoutnav': {
-    'type': 'integer',
-  },
-  'device': {
     'type': 'string',
   },
   'pub_date': {
     'type': 'string',
   },
-  'story_template_prefix_navigation_option': {
+  'category_id': {
+    'type': 'string',
+  },
+  'lastUpdateBy': {
     'type': 'string',
   },
   'url': {
@@ -438,13 +116,7 @@ schema = {
   },
   'protected_tags': {
     'type': 'list',
-  },
-  'title_page_iphone': {
-    'type': 'string',
-  },
-  'category_id': {
-    'type': 'string',
-  },
+  }
 }
 
 article = {
@@ -456,6 +128,7 @@ article = {
     'resource_methods': ['GET', 'POST'],
     'cache_control': 'max-age=300,must-revalidate',
     'cache_expires': 300,
+    'allow_unknown': True,
     'schema': schema
 }
 
