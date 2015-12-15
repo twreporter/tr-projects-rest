23bb5b82acc6708596094dcdb83cf8e3ac3d883d
nickhsine <nickhsine97753017@gmail.com>
2015-12-15 22:41:07 +0800

    add comment

--- script/cron_feed_article.sh
+++ script/cron_feed_article.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# get full directory name of the script no matter where it is being called from
 file_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $file_path
 echo 'fetching articles'
