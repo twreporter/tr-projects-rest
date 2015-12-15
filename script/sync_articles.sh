8233f46bc17f69ac897c83fc1bb9758e3e237984
drumrick <me@drumrick.tw>
2015-12-15 14:18:06 +0000

    force remove after uploading

--- script/sync_articles.sh
+++ script/sync_articles.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-`mkdir -p /tmp/twreporters/articles`
+
 `gsutil -m -h "Content-Type:text/html" -h "Cache-Control:max-age=3600,public" -h "Content-Language:zh" cp -z html -a public-read /tmp/twreporters/articles/* gs://twreporter-article.twreporter.org`
-`rm /tmp/twreporters/articles/*`
+`rm -f /tmp/twreporters/articles/*`
