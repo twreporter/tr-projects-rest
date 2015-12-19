b4b03e3c03fa9bceede25a94540fa08784c2f1f1
Rick Liu <me@drumrick.tw>
2015-12-20 06:33:27 +0800

    script move to https://github.com/twreporter/tr-projects-crontab

--- script/sync_articles.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-`gsutil -m -h "Content-Type:text/html" -h "Cache-Control:max-age=3600,public" -h "Content-Language:zh" cp -z html -a public-read /tmp/twreporters/articles/* gs://twreporter-article.twreporter.org`
-`rm -f /tmp/twreporters/articles/*`
