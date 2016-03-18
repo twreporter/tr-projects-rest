8bb46001b187e889742423b19db92f55802240f4
ChienHsin-Chan <hcchien@gmail.com>
2016-03-18 16:48:14 +0800

    remove the tag endpoint

--- server.py
+++ server.py
@@ -7,10 +7,6 @@ app = Eve()
 app.on_replace_article += lambda item, original: remove_extra_fields(item)
 app.on_insert_article += lambda items: remove_extra_fields(items[0])
 
-@app.route("/tag/<name>", methods=['GET'])
-def tagSearch(name):
-  return redirect('/posts/?where={"tags":{"$in":["%s"]}}' % name, code=302)
-
 def remove_extra_fields(item):
   accepted_fields = schema.keys()
   for field in item.keys():
