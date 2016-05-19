a63fdb7a64f0cdadc6d778ac3bd910e589955695
ChienHsin-Chan <hcchien@gmail.com>
2016-05-19 18:51:13 +0800

    add some default fields for the posts

--- settings.sample.py
+++ settings.sample.py
@@ -350,6 +350,7 @@ posts = {
         'filter': {'state': 'published'},
     },
     'resource_methods': ['GET'],
+    'embedded_fields': ['writters','photographers','designers','engineers','heroImage'],
     'cache_control': 'max-age=300,must-revalidate',
     'cache_expires': 300,
     'allow_unknown': False,
